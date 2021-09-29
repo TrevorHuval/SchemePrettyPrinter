1bdce0296c5d557d1e740d7fd2e8068a37035d98
theblakelalonde <78009242+theblakelalonde@users.noreply.github.com>
2021-09-29 01:05:54 -0500

    Add files via upload

--- Parse/Scanner.py
+++ Parse/Scanner.py
@@ -4,6 +4,7 @@ import sys
 import io
 from Tokens import *
 
+
 class Scanner:
     def __init__(self, i):
         self.In = i
@@ -17,7 +18,7 @@ class Scanner:
             ch = self.ch_buf
             self.ch_buf = None
             return ch
-    
+
     def peek(self):
         if self.ch_buf == None:
             self.ch_buf = self.In.read(1)
@@ -29,6 +30,32 @@ class Scanner:
     def isDigit(ch):
         return ch >= '0' and ch <= '9'
 
+    @staticmethod
+    def isLetter(ch):
+        return ch >= 'a' and ch <= 'z'
+
+    @staticmethod
+    def isSpecialInitial(ch):
+        return ch == '!' or ch == '$' or ch == '%' or ch == '&' or ch == '*' or ch == '/' or ch == ':' or ch == '<' or ch == '=' or ch == '>' or ch == '?' or ch == '^' or ch == '_' or ch == '`'
+
+    @staticmethod
+    def isSpecialSub(ch):
+        return ch == '+' or ch == '-' or ch == '.' or ch == '@'
+
+    @staticmethod
+    def isPeculiarIdentifier(ch):
+        return ch == '+' or ch == '-'
+
+    @staticmethod
+    def isInitial(ch):
+        # return ch.isLetter() or ch.isSpecialInitial()
+        return (ch >= 'a' and ch <= 'z') or (ch == '!' or ch == '$' or ch == '%' or ch == '&' or ch == '*' or ch == '/' or ch == ':' or ch == '<' or ch == '=' or ch == '>' or ch == '?' or ch == '^' or ch == '_' or ch == '`')
+
+    @staticmethod
+    def isSubsequent(ch):
+        # return ch.isIntial() or ch.isDigit() or ch.specialSub()
+        return ((ch >= 'a' and ch <= 'z') or (ch == '!' or ch == '$' or ch == '%' or ch == '&' or ch == '*' or ch == '/' or ch == ':' or ch == '<' or ch == '=' or ch == '>' or ch == '?' or ch == '^' or ch == '_' or ch == '`')) or (ch >= '0' and ch <= '9') or (ch == '+' or ch == '-' or ch == '.' or ch == '@')
+
     def getNextToken(self):
         try:
             # It would be more efficient if we'd maintain our own
@@ -37,24 +64,25 @@ class Scanner:
             # input stream is easier.
             ch = self.read()
 
-            # TODO: Skip white space and comments
+            # HOPEFULLY: Skip white space and comments
             loopBool = False
             while(loopBool == False):
-                if(ch == ' ' or '\t' or '\n' or '\r'):
-                    return self.getNextToken
+                if(ch == ' ' or ch == '\t' or ch == '\r' or ch == '\n'):
+                    return self.getNextToken()
                 elif(ch == ';'):
-                    
-                    
-                        
+                    commentLoop = False
+                    while(commentLoop == False):
+                        ch = self.read()
+                        if(ch == '\r' or ch == '\n'):
+                            commentLoop = True
+                            return self.getNextToken()
                 else:
                     loopBool = True
-
-
 
             # Return None on EOF
             if ch == "":
                 return None
-    
+
             # Special characters
             elif ch == '\'':
                 return Token(TokenType.QUOTE)
@@ -85,26 +113,55 @@ class Scanner:
             # String constants
             elif ch == '"':
                 self.buf = []
-                # TODO: scan a string into the buffer variable buf
-    
+                # HOPEFULLY: scan a string into the buffer variable buf
+                stringLoop = False
+                while (stringLoop == False):
+                    ch = self.read()
+                    if (ch != '"'):
+                        self.buf.append(ch)
+                    else:
+                        stringLoop = True
+
                 return StrToken("".join(self.buf))
 
             # Integer constants
             elif self.isDigit(ch):
                 i = ord(ch) - ord('0')
-                # TODO: scan the number and convert it to an integer
+
+                # HOPEFULLY: scan the number and convert it to an integer
+                curVal = i
+                intLoop = False
+                while (intLoop == False):
+                    if (self.isDigit(self.peek())):
+                        ch = self.read()
+                        i = ord(ch) - ord('0')
+                        curVal = (curVal * 10) + i
+                    else:
+                        intLoop = True
 
                 # make sure that the character following the integer
                 # is not removed from the input stream
-                return IntToken(i)
-    
+                return IntToken(curVal)
+
             # Identifiers
-            elif ch >= 'A' and ch <= 'Z':
-                # or ch is some other vaid first character
+            elif ((ch >= 'A' and ch <= 'Z') or self.isInitial(ch) or self.isPeculiarIdentifier(ch)):
+                # or ch is some other valid first character
                 # for an identifier
                 self.buf = []
-                # TODO: scan an identifier into the buffer variable buf
 
+                if self.isInitial(ch):
+                    self.buf.append(ch)
+                    ch = self.read()
+                    while(self.isSubsequent(ch)):
+                        self.buf.append(ch)
+                        if(self.isSubsequent(self.peek())):
+                            ch = self.read()
+                        else:
+                            break
+                elif self.isPeculiarIdentifier(ch):
+                    self.buf.append(ch)
+
+                # HOPEFULLY: scan an identifier into the buffer variable buf
 
                 # make sure that the character following the identifier
                 # is not removed from the input stream
